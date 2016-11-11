772519faa5c54c07e894118568b6ad34ef9bdcc2
showzyl <showzyl520@gmail.com>
2016-11-11 13:56:30 +0800

    add

---
+++ src/page/Order.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="order">
+  我是`order`
+</div>
+</template>
+<style media="screen" scoped>
+  .order{
+
+  }
+
+</style>
+<script lang="babel">
+import commonNav from '../components/commonNav.vue'
+
+export default {
+  components: {
+    
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+  watch: {
+
+  }
+}
+</script>
