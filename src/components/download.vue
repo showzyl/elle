73c546329a3c6bc36384a44cb4e12abd90b3d3ad
showzyl <showzyl520@gmail.com>
2017-03-04 01:23:33 +0800

    recover header
    recover header

--- src/components/download.vue
+++ src/components/download.vue
@@ -1,41 +1,41 @@
 <template>
-  <!--<div class="container">-->
-    <!--<div class="buyer-head clearfloat" >-->
-    <!--<div class="fl">-->
-      <!--&lt;!&ndash;<img class="buyer-close" id="buyer_close" src="//www.elleshop.com.cn/catalog/view/theme/mobile/image/buyer-close.jpg">&ndash;&gt;-->
-    <!--</div>-->
-    <!--<div class="fl buyer-head-main">-->
-      <!--<div class="clearfloat">-->
-        <!--<div class="fl">-->
-          <!--<img class="buyer-logo" src="../assets/img/logo.png">-->
-        <!--</div>-->
-        <!--<div class="fl">-->
-          <!--<p>ELLESHOP - 我的私人买手</p>-->
+  <div class="container">
+    <div class="buyer-head clearfloat" >
+    <div class="fl">
+      <!--<img class="buyer-close" id="buyer_close" src="//www.elleshop.com.cn/catalog/view/theme/mobile/image/buyer-close.jpg">-->
+    </div>
+    <div class="fl buyer-head-main">
+      <div class="clearfloat">
+        <div class="fl">
+          <img class="buyer-logo" src="../assets/img/logo.png">
+        </div>
+        <div class="fl">
+          <p>ELLESHOP - 我的私人买手</p>
           <!--<p class="elleshop">ELLESHOP</p>-->
-          <!--<p class="buyer-star-w"><img class="buyer-star" src="//www.elleshop.com.cn/catalog/view/theme/mobile/image/buyer-star.jpg"></p>-->
-          <!--<p class="elleshop">获取 - App</p>-->
-        <!--</div>-->
-      <!--</div>-->
-    <!--</div>-->
-    <!--<div class="fr">-->
-      <!--<p class="head-xs" id="iphoneBtn" @click="download"><a href="javascript:;">下载APP</a></p>-->
-    <!--</div>-->
-  <!--</div>-->
-  <!--</div>-->
-  
-  <div class="download">
-    <div class="logo">
-      <img class="buyer-logo" src="../assets/img/logo.png">
+          <p class="buyer-star-w"><img class="buyer-star" src="//www.elleshop.com.cn/catalog/view/theme/mobile/image/buyer-star.jpg"></p>
+          <p class="elleshop">获取 - App</p>
+        </div>
+      </div>
     </div>
-
-    <div class="txt">
-      <h3>领取1500元现金礼券</h3>
-      <p>ELLESHOP－我的私人买手</p>
+    <div class="fr">
+      <p class="head-xs" id="iphoneBtn" @click="download"><a href="javascript:;">下载APP</a></p>
     </div>
-
-    <div class="btn-down" @click="download"></div>
-
   </div>
+  </div>
+  
+  <!--<div class="download">-->
+    <!--<div class="logo">-->
+      <!--<img class="buyer-logo" src="../assets/img/logo.png">-->
+    <!--</div>-->
+
+    <!--<div class="txt">-->
+      <!--<h3>领取1500元现金礼券</h3>-->
+      <!--<p>ELLESHOP－我的私人买手</p>-->
+    <!--</div>-->
+
+    <!--<div class="btn-down" @click="download"></div>-->
+
+  <!--</div>-->
   
 </template>
 
@@ -86,70 +86,78 @@
     top: .7rem;
   }
 
-  /**/
-  /**/
-  /*.container{*/
-    /*position: fixed;*/
-    /*top: 0;*/
-    /*left: 0;*/
-    /*background-color: #ffffff;*/
-    /*width: 100%;*/
-    /*z-index: 999999999;*/
-  /*}*/
-  /*.buyer-head {*/
-    /*!*padding: .20129rem .44283rem .20129rem .20129rem;*!*/
-    /*padding: .20129rem .44283rem;*/
-    /*border-bottom: 1px solid #d7d7d5;*/
-    /*overflow: hidden;*/
-  /*}*/
 
-  /*.clearfloat {*/
-    /*zoom: 1;*/
-  /*}*/
 
-  /*.buyer-head>div:first-child {*/
-    /*height: 1.61031rem;*/
-    /*line-height: 1.61031rem;*/
-  /*}*/
+  .container{
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: #ffffff;
+    width: 100%;
+    z-index: 999999999;
+    height: 2rem;
+    overflow: hidden;
+    border-bottom: 1px solid #d7d7d5;
 
-  /*.buyer-close {*/
-    /*width: .2818rem;*/
-    /*height: .2818rem;*/
-    /*margin-right: .24155rem;*/
-    /*cursor: pointer;*/
-  /*}*/
+  }
+  .buyer-head {
+    /*padding: .20129rem .44283rem .20129rem .20129rem;*/
+    padding: .20129rem .44283rem;
+    border-bottom: 1px solid #d7d7d5;
+    overflow: hidden;
+  }
 
-  /**/
+  .clearfloat {
+    zoom: 1;
+  }
 
-  /*.fl {*/
-    /*float: left;*/
-  /*}*/
+  .buyer-head>div:first-child {
+    height: 1.61031rem;
+    line-height: 1.61031rem;
+  }
 
-  /*.fr {*/
-    /*float: right;*/
-  /*}*/
-  /*.buyer-head-main p {*/
-    /*font-family: "PingFangSC Medium", "微软雅黑","Microsoft Yahei";*/
-    /*font-size: .28986rem;*/
-  /*}*/
+  .buyer-close {
+    width: .2818rem;
+    height: .2818rem;
+    margin-right: .24155rem;
+    cursor: pointer;
+  }
 
-  /*.head-xs a {*/
-    /*font-family: "PingFangSC Medium", "微软雅黑","Microsoft Yahei";*/
-    /*font-size: .43478rem;*/
-    /*color: #1f1f1f;*/
-    /*font-weight: bold;*/
-    /*height: 1.61031rem;*/
-    /*line-height: 1.61031rem;*/
-    /*!*border: 1px solid;*!*/
-    /*!*border-radius: 50%;*!*/
-    /*!*padding: .3rem;*!*/
-  /*}*/
 
-  /*.buyer-star {*/
-    /*width: 1.24799rem;*/
-    /*height: .24155rem;*/
-  /*}*/
+  .fl {
+    float: left;
+  }
 
+  .fr {
+    float: right;
+  }
+  .buyer-head-main p {
+    font-family: "PingFangSC Medium", "微软雅黑","Microsoft Yahei";
+    font-size: .28986rem;
+  }
+
+  .head-xs a {
+    font-family: "PingFangSC Medium", "微软雅黑","Microsoft Yahei";
+    font-size: .43478rem;
+    color: #1f1f1f;
+    font-weight: bold;
+    height: 1.61031rem;
+    line-height: 1.61031rem;
+    /*border: 1px solid;*/
+    border-radius: 50%;
+    padding: .3rem;
+  }
+
+  .buyer-star {
+    width: 1.24799rem;
+    height: .24155rem;
+  }
+
+  .buyer-logo {
+    width: 1.61031rem;
+    height: 1.61031rem;
+    margin-right: .12077rem;
+  }
 
 </style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,60 +3,60 @@
 import Index from '../page/Index.vue'
 
 
-import Shopcar from '../page/Shopcar.vue'
-import ConfirmOrder from '../page/ConfirmOrder.vue'
-import Pay from '../page/Pay.vue'
-import Profile from '../page/Profile.vue'
-import Personal from '../page/Personal.vue'
-import Classify from '../page/Classify.vue'
-import Inspire from '../page/Inspire.vue'
-import InspireDetail from '../page/InspireDetail.vue'
-import Address from '../page/Address.vue'
-import AddressDetail from '../page/AddressDetail.vue'
-import Invoice from '../page/Invoice.vue'
-import InvoiceDetail from '../page/InvoiceDetail.vue'
-import Collect from '../page/Collect.vue'
-import Order from '../page/Order.vue'
-import Product from '../page/Product.vue'
-import Filter from '../page/Filter.vue'
-import Brand from '../page/Brand.vue'
-import Login from '../page/Login.vue'
-import TelReg from '../page/TelReg.vue'
-import Findpwd from '../page/Findpwd.vue'
-import Changepwd from '../page/Changepwd.vue'
-import History from '../page/History.vue'
-import Coupon from '../page/Coupon.vue'
-import Vip from '../page/Vip.vue'
-import Qa from '../page/Qa.vue'
-import Test from '../page/Test.vue'
+// import Shopcar from '../page/Shopcar.vue'
+// import ConfirmOrder from '../page/ConfirmOrder.vue'
+// import Pay from '../page/Pay.vue'
+// import Profile from '../page/Profile.vue'
+// import Personal from '../page/Personal.vue'
+// import Classify from '../page/Classify.vue'
+// import Inspire from '../page/Inspire.vue'
+// import InspireDetail from '../page/InspireDetail.vue'
+// import Address from '../page/Address.vue'
+// import AddressDetail from '../page/AddressDetail.vue'
+// import Invoice from '../page/Invoice.vue'
+// import InvoiceDetail from '../page/InvoiceDetail.vue'
+// import Collect from '../page/Collect.vue'
+// import Order from '../page/Order.vue'
+// import Product from '../page/Product.vue'
+// import Filter from '../page/Filter.vue'
+// import Brand from '../page/Brand.vue'
+// import Login from '../page/Login.vue'
+// import TelReg from '../page/TelReg.vue'
+// import Findpwd from '../page/Findpwd.vue'
+// import Changepwd from '../page/Changepwd.vue'
+// import History from '../page/History.vue'
+// import Coupon from '../page/Coupon.vue'
+// import Vip from '../page/Vip.vue'
+// import Qa from '../page/Qa.vue'
+// import Test from '../page/Test.vue'
 
 
-// const Shopcar = () => System.import('../page/Shopcar.vue')
-// const ConfirmOrder = () => System.import('../page/ConfirmOrder.vue')
-// const Pay = () => System.import('../page/Pay.vue')
-// const Profile = () => System.import('../page/Profile.vue')
-// const Personal = () => System.import('../page/Personal.vue')
-// const Classify = () => System.import('../page/Classify.vue')
-// const Inspire = () => System.import('../page/Inspire.vue')
-// const InspireDetail = () => System.import('../page/InspireDetail.vue')
-// const Address = () => System.import('../page/Address.vue')
-// const AddressDetail = () => System.import('../page/AddressDetail.vue')
-// const Invoice = () => System.import('../page/Invoice.vue')
-// const InvoiceDetail = () => System.import('../page/InvoiceDetail.vue')
-// const Collect = () => System.import('../page/Collect.vue')
-// const Order = () => System.import('../page/Order.vue')
-// const Product = () => System.import('../page/Product.vue')
-// const Filter = () => System.import('../page/Filter.vue')
-// const Brand = () => System.import('../page/Brand.vue')
-// const Login = () => System.import('../page/Login.vue')
-// const TelReg = () => System.import('../page/TelReg.vue')
-// const Findpwd = () => System.import('../page/Findpwd.vue')
-// const Changepwd = () => System.import('../page/Changepwd.vue')
-// const History = () => System.import('../page/History.vue')
-// const Coupon = () => System.import('../page/Coupon.vue')
-// const Qa = () => System.import('../page/Qa.vue')
-// const Vip = () => System.import('../page/Vip.vue')
-// const Test = () => System.import('../page/Test.vue')
+const Shopcar = () => System.import('../page/Shopcar.vue')
+const ConfirmOrder = () => System.import('../page/ConfirmOrder.vue')
+const Pay = () => System.import('../page/Pay.vue')
+const Profile = () => System.import('../page/Profile.vue')
+const Personal = () => System.import('../page/Personal.vue')
+const Classify = () => System.import('../page/Classify.vue')
+const Inspire = () => System.import('../page/Inspire.vue')
+const InspireDetail = () => System.import('../page/InspireDetail.vue')
+const Address = () => System.import('../page/Address.vue')
+const AddressDetail = () => System.import('../page/AddressDetail.vue')
+const Invoice = () => System.import('../page/Invoice.vue')
+const InvoiceDetail = () => System.import('../page/InvoiceDetail.vue')
+const Collect = () => System.import('../page/Collect.vue')
+const Order = () => System.import('../page/Order.vue')
+const Product = () => System.import('../page/Product.vue')
+const Filter = () => System.import('../page/Filter.vue')
+const Brand = () => System.import('../page/Brand.vue')
+const Login = () => System.import('../page/Login.vue')
+const TelReg = () => System.import('../page/TelReg.vue')
+const Findpwd = () => System.import('../page/Findpwd.vue')
+const Changepwd = () => System.import('../page/Changepwd.vue')
+const History = () => System.import('../page/History.vue')
+const Coupon = () => System.import('../page/Coupon.vue')
+const Qa = () => System.import('../page/Qa.vue')
+const Vip = () => System.import('../page/Vip.vue')
+const Test = () => System.import('../page/Test.vue')
 
 
 const routes = [
